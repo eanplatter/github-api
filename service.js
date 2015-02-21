04a9021b70db10149bfee3065ea4faf3e1522f4a
eanplatter <ean.platter@gmail.com>
2015-02-21 10:36:56 -0700

    add api keys for github

--- service.js
+++ service.js
@@ -1,24 +1,29 @@
 var app = angular.module('github-api');
 
-app.service('gitHubService', function($http) {
+app.service('gitHubService', function($http, env) {
+
+  var id = env.id;
+  var sec = env.secret;
+  var param = "?client_id=" + id + "&client_secret=" + sec;
+
   this.getUser = function(username) {
     return $http({
       method: 'GET',
-      url: 'https://api.github.com/users/' + username
+      url: 'https://api.github.com/users/' + username + param
     })
   }
 
   this.getUserRepos = function(username) {
     return $http({
       method: 'GET',
-      url: 'https://api.github.com/users/' + username + '/repos'
+      url: 'https://api.github.com/users/' + username + '/repos' + param
     })
   }
 
   this.getRateLimit = function() {
     return $http({
       method: 'GET',
-      url: 'https://api.github.com/rate_limit'
+      url: 'https://api.github.com/rate_limit' + param
     })
   }
 });
